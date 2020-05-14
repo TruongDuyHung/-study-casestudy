78bbf4e83ffe3a9dc48255fb459cba1f2b15b7cd
fire <gioihannaochochungta9x@gmail.com>
2020-05-14 10:13:16 +0700

    change view and css form

--- view/form-login.php
+++ view/form-login.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="css/form-login.css">
+    <link rel="stylesheet" href="css/css.css">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -34,17 +34,17 @@
                     <span class="glyphicon glyphicon-user"></span>
                     User
                 </button>
-                <input type="text" placeholder="Email" name="email">
-                <input type="password" placeholder="Password" name="password">
+                <input type="email" placeholder="Email" name="email" required>
+                <input type="password" placeholder="Password" name="password" required>
                 <input type="submit" value="Login">
             </div>
         </form>
         <form method="GET">
             <div class="register-show">
                 <h2>REGISTER</h2>
-                <input type="text" placeholder="Email" name="email1">
-                <input type="text" placeholder="Full name" name="fullname">
-                <input type="password" placeholder="Password" name="password1">
+                <input type="email" placeholder="Email" name="email1" required>
+                <input type="text" placeholder="Full name" name="fullname" required>
+                <input type="password" placeholder="Password" name="password1" required>
                 <input type="submit" value="Register">
             </div>
         </form>
